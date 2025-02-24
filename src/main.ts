4f650a1114f51c91dd20e88ce94160d63681a51d
chansik0504 <chansik0504@gmail.com>
2025-02-24 19:14:44 +0900

    나 권수혁인데 나 일베한다

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   //서버 시작지점
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: ['https://travelevart.com', 'https://travelevart.netlify.app']});
   await app.listen(3000);
 }
 bootstrap();
